6c1c78bd412bc5a612984d4210ab2af2b6ffaddd
Noah Kogler <noah.kogler@gmail.com>
2021-04-25 20:15:13 +0200

    positions around center

--- components/skill-map.js
+++ components/skill-map.js
@@ -13,11 +13,25 @@ export default function SkillMap() {
 
   const padding = 20;
   const sideLength = 100;
+  const halfHeight = hexHalfHeight(sideLength);
+
   const central = {x: width / 2, y: 300 };
+  const topLeft = {x: central.x - 3 * sideLength / 2, y: central.y - halfHeight };
+  const top = {x: central.x, y: central.y - 2 * halfHeight };
+  const topRight = {x: central.x + 3 * sideLength / 2, y: central.y - halfHeight };
+  const bottomRight = {x: central.x + 3 * sideLength / 2, y: central.y + halfHeight };
+  const bottom = {x: central.x, y: central.y + 2 * halfHeight };
+  const bottomLeft = {x: central.x - 3 * sideLength / 2, y: central.y + halfHeight };
+
   return (
     <Container ref={ref}>
       <Hexagon center={central} sideLength={sideLength} padding={padding}/>
-      <Hexagon center={{x: central.x - 3 * sideLength / 2, y: central.y - hexHalfHeight(sideLength) }} sideLength={sideLength}  padding={padding}/>
+      <Hexagon center={topLeft} sideLength={sideLength}  padding={padding}/>
+      <Hexagon center={top} sideLength={sideLength}  padding={padding}/>
+      <Hexagon center={topRight} sideLength={sideLength}  padding={padding}/>
+      <Hexagon center={bottomRight} sideLength={sideLength}  padding={padding}/>
+      <Hexagon center={bottom} sideLength={sideLength}  padding={padding}/>
+      <Hexagon center={bottomLeft} sideLength={sideLength}  padding={padding}/>
     </Container>
   );
 }
