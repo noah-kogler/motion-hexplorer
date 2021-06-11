8ba1e226199765ffef4123ddfeb2811fde8f8117
Noah Kogler <noah.kogler@gmail.com>
2021-06-11 12:00:22 +0200

    added background and fonts

--- components/header.js
+++ components/header.js
@@ -10,13 +10,17 @@ export default function Header({heightRem}) {
 
 const Container = styled.div`
   height: ${props => props.heightRem}rem;
-  border-bottom: 2px solid black;
+  border-bottom: 2px solid #333333;
   box-sizing: border-box;
+  background: rgba(255, 255, 255, 0.75);
+  box-shadow: 0 0 15px #000000;
 `;
 
 const Title = styled.h1`
   padding: 1rem;
   margin: 0;
-  font-family: sans-serif;
+  padding-top: 2rem;
+  font-family: 'M PLUS Rounded 1c', sans-serif;
   text-align: center;
+  color: #333333;
 `;
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Header from "../components/header";
 
 export default function Index () {
   const [showSkillMap, setShowSkillMap] = useState(false);
-  const headerHeightRem = 5;
+  const headerHeightRem = 7;
 
   // Wait until after client-side hydration to show the map, because useWindowSize is only available on the client.
   useEffect(() => {
@@ -54,11 +54,43 @@ export default function Index () {
 };
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'M PLUS Rounded 1c';
+    font-style: normal;
+    font-weight: 500;
+    src: url('/fonts/m-plus-rounded-1c-v10-latin-ext_latin/m-plus-rounded-1c-v10-latin-ext_latin-500.eot'); /* IE9 Compat Modes */
+    src: local(''),
+         url('/fonts/m-plus-rounded-1c-v10-latin-ext_latin/m-plus-rounded-1c-v10-latin-ext_latin-500.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('/fonts/m-plus-rounded-1c-v10-latin-ext_latin/m-plus-rounded-1c-v10-latin-ext_latin-500.woff2') format('woff2'), /* Super Modern Browsers */
+         url('/fonts/m-plus-rounded-1c-v10-latin-ext_latin/m-plus-rounded-1c-v10-latin-ext_latin-500.woff') format('woff'), /* Modern Browsers */
+         url('/fonts/m-plus-rounded-1c-v10-latin-ext_latin/m-plus-rounded-1c-v10-latin-ext_latin-500.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('/fonts/m-plus-rounded-1c-v10-latin-ext_latin/m-plus-rounded-1c-v10-latin-ext_latin-500.svg#MPLUSRounded1c') format('svg'); /* Legacy iOS */
+  }
+  
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    src: url('/fonts/roboto-v27-latin-ext_latin/roboto-v27-latin-ext_latin-regular.eot'); /* IE9 Compat Modes */
+    src: local(''),
+         url('/fonts/roboto-v27-latin-ext_latin/roboto-v27-latin-ext_latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('/fonts/roboto-v27-latin-ext_latin/roboto-v27-latin-ext_latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+         url('/fonts/roboto-v27-latin-ext_latin/roboto-v27-latin-ext_latin-regular.woff') format('woff'), /* Modern Browsers */
+         url('/fonts/roboto-v27-latin-ext_latin/roboto-v27-latin-ext_latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('/fonts/roboto-v27-latin-ext_latin/roboto-v27-latin-ext_latin-regular.svg#Roboto') format('svg'); /* Legacy iOS */
+  }
+
   html, body {
     height: 100%;
     width: 100%;
     margin: 0;
     padding: 0;
+  }
+  
+  body {
+    background: url('/background.png');
+    background-size: cover;
+    font-family: 'Roboto', sans-serif;
   }
 `;
 
