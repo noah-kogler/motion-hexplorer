dddfddaba04e67883945ba86361b708c63604d66
Noah Kogler <noah.kogler@gmail.com>
2021-06-22 20:54:27 +0200

    open the correct marker for lesson locations

--- components/location-map.js
+++ components/location-map.js
@@ -1,11 +1,23 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { withTheme } from "@material-ui/core";
 import useContentAreaSize from "../hooks/content-area-size";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import { withTheme } from "@material-ui/core";
+import { useEffect, useRef } from "react";
 
 L.Icon.Default.imagePath='icons/leaflet/';
+
+const OpenMarker = props => {
+  const leafletRef = useRef();
+
+  useEffect(() => {
+    leafletRef.current.openPopup();
+  },[])
+
+  return <Marker ref={leafletRef} {...props} />
+}
 
 function LocationMap({theme, lessons}) {
   const router = useRouter();
@@ -13,8 +25,6 @@ function LocationMap({theme, lessons}) {
   if (width === 0 && height === 0) {
     return <div>loading...</div>
   }
-
-  console.log('TODO MARK: ' + router.query.markLessonId);
 
   const salzburg = [47.8095, 13.0550];
 
@@ -31,14 +41,21 @@ function LocationMap({theme, lessons}) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {
-          lessonsWithLocations.map(lesson =>
-            <Marker position={lesson.location.coords} key={lesson.id}>
-              <Popup>
-                {lesson.title}<br/>
-                {lesson.category}
-              </Popup>
-            </Marker>
-          )
+          lessonsWithLocations.map(lesson => {
+            const ChosenMarker = lesson.id === router.query.markLessonId ? OpenMarker : Marker;
+            return <ChosenMarker
+              position={lesson.location.coords}
+              key={lesson.id}>
+                <Popup>
+                  {lesson.title} @ {lesson.location.name}<br/>
+                  {lesson.category}
+                  <Image
+                    width={150}
+                    height={100}
+                    src={'image' in lesson ? lesson.image : '/placeholder.svg'} />
+                </Popup>
+            </ChosenMarker>;
+          })
         }
       </MapContainer>
     </div>
