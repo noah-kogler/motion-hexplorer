e8f3a3be5c823c21e9cf6dabe27fa7c50101bf9f
Noah Kogler <noah.kogler@gmail.com>
2021-06-27 12:39:05 +0200

    improved location map styling

--- components/header.js
+++ components/header.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles({
     background: 'rgba(255, 255, 255, 0.75)',
     boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)',
     position: "relative",
-    zIndex: '10',
+    zIndex: '1000',
     padding: '0 2rem',
   },
   title: {
--- components/location-map.js
+++ components/location-map.js
@@ -38,7 +38,7 @@ function LocationMap({theme, lessons}) {
         style={{height: '100%'}}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         />
         {
           lessonsWithLocations.map(lesson => {
