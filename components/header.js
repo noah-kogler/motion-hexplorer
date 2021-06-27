23bb5ecaa2befdad34d16e0554dd08688d0427d1
Noah Kogler <noah.kogler@gmail.com>
2021-06-27 15:40:27 +0200

    minor styling improvements

--- components/header.js
+++ components/header.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles({
     boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)',
     position: "relative",
     zIndex: '1000',
-    padding: '0 2rem',
+    padding: '0 .4rem',
   },
   title: {
     padding: '1rem',
